310bf826b4168135f38d84f74789570df6e1f684
Michelle Ross <miross629@gmail.com>
2023-02-09 09:56:17 -0800

    Clicking an item adds to shopping list

--- src/Components/ShoppingList.js
+++ src/Components/ShoppingList.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { produce } from "../list.js";
 import ItemsList from "./ItemsList.js";
 
 export default function ShoppingList() {
-  // TODO declare and initialize state
+  // declare and initialize state
+  const [shoppingList, setShoppingList] = useState([]);
 
-  // TODO event handler for adding item to shopping list
+  // event handler for adding item to shopping list
+  const addItem = (item) => {
+    setShoppingList((prev) => {
+      return [item, ...prev];
+    });
+  };
 
   // TODO event handler for removing item from shopping list
 
@@ -13,14 +19,19 @@ export default function ShoppingList() {
     <>
       <h1>Shopping List</h1>
       <p>Select an item to remove from shopping list.</p>
-      {/* TODO display added items as a list using .map */}
+      {/* display added items as a list using .map */}
+      <ul>
+        {shoppingList.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
 
       <h2>Add Items</h2>
       <p>Select an item to add to shopping list.</p>
 
       <h3>Produce</h3>
       {/* call ItemsList to display produce options  */}
-      <ItemsList items={produce} />
+      <ItemsList items={produce} onItemClick={addItem} />
     </>
   );
 }
