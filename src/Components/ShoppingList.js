b343a93b367dd2645e53093487c6eed1a1a1a67f
Michelle Ross <miross629@gmail.com>
2023-02-08 16:20:27 -0800

    Display initial framework of ShoppingList in App

---
+++ src/Components/ShoppingList.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function ShoppingList() {
+  // TODO declare and initialize state
+
+  // TODO event handler for adding item to shopping list
+
+  // TODO event handler for removing item from shopping list
+
+  return (
+    <>
+      <h1>Shopping List</h1>
+      <p>Select an item to remove from shopping list.</p>
+      {/* TODO display added items as a list using .map */}
+
+      <h2>Add Items</h2>
+      <p>Select an item to add to shopping list.</p>
+      <h3>Produce</h3>
+      {/* TODO call ItemsList to display produce options  */}
+    </>
+  );
+}
